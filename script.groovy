b0926371d8dde093b32704401401e865b817003b
Glarez <germanlarez@gmail.com>
2021-07-20 21:19:18 -0400

    Revert "Revert "Added logic trough groovy script and renamed Jenkins decalrative way file to spam a new one IV Test choice added"" Reverted to the last wrking commit This reverts commit 97cd452fa68fb45254c02a731ff6b930909a909c.

--- script.groovy
+++ script.groovy
@@ -1,19 +1,19 @@
 def buildApp() {
+    
     echo "Building the app ${params.VERSION}"
 
 }
 
 def testApp() {
+
     echo "Testing the app"
-    when {
-                expression{
-                    params.executeTest == true // this will execute test if in the section parameters/boolean above the default is true
-                }
 
 }
 
 def deployApp() {
+
     echo "Deploying the app ${params.VERSION}"
 
 }
+
 return this
