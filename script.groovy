558dc3a912cb3272a2b8e451c409f4e42d3f8a7e
Glarez <germanlarez@gmail.com>
2021-07-21 21:37:38 -0400

    full pipeline config for Java Maven App from Groovy script

---
+++ script.groovy
@@ -0,0 +1,17 @@
+def buildJar() {
+        echo "========executing Maven Build========"
+        sh "mvn package"
+}
+
+def buildImage() {
+          echo "========Building the Docker image========"
+            withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            sh 'docker build -t glarez/java-maven-app:1.0 .'
+            echo "echo $PASS | docker login -u $USER --password-stdin"
+            sh 'docker push glarez/java-maven-app:1.0'
+            }
+}
+
+def deployApp() {
+        echo "========Deploying Java App========"
+}
